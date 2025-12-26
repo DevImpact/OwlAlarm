422c240565e5ebb8bcd14bacc92adb76eede12e1
Dev Impact <shahra.admo@gmail.com>
2025-12-26 20:07:31 +0100

    Delete app directory

--- app/src/main/java/com/owlalarm/odesa/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.owlalarm.odesa;
-
-public abstract class Constants {
-    public static final String HOUR = "time_hour";
-    public static final String MINUTE = "time_minute";
-    public static final String TIME_PICKER = "time_picker";
-}
